c280d50ab4d99d4708c604894665e37490f53507
Emil Andersen <emil.rauff.andersen@hotmail.com>
2023-04-17 19:21:39 +0200

    Added unit test for IFFT

--- Source/UnitTest/FastFourierTransformTest.cpp
+++ Source/UnitTest/FastFourierTransformTest.cpp
@@ -41,4 +41,23 @@ namespace UnitTest {
             EXPECT_NEAR(imagine_buffer[i], expected_imagine[i], 1e-5);
         }
     }
+
+    TEST(FFTShould, ExpectRealNumbers_When_InvokingIFFT) {
+        // Arrange
+        std::vector<float> real_buffer      = { 10.00000, -2.000000, -2.000000, -2.000000 };
+        std::vector<float> imagine_buffer   = { 0.000000, 2.000000, 0.000000, -2.000000 };
+
+        std::vector<float> expected_real = { 1, 2, 3, 4 };
+        int expected_size = 4;
+
+        // Act
+        FFTUtility::IFFT(real_buffer, imagine_buffer);
+
+        // Assert
+        // Check that the output matches the expected result
+        ASSERT_EQ(real_buffer.size(), expected_size);
+        for (int i = 0; i < expected_size; i++) {
+            EXPECT_NEAR(real_buffer[i], expected_real[i], 1e-5);
+        }
+    }
 }
