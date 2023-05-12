3ddcf9cfe64800f6004b963e64d87c8f77bf6143
EADR (Emil Andersen) <eadr@nnit.com>
2023-05-12 09:28:22 +0200

    Added some sound from waveout

--- Source/AmpProcessing/Devices/SimpleDebugDevice.cpp
+++ Source/AmpProcessing/Devices/SimpleDebugDevice.cpp
@@ -1,12 +1,28 @@
 #include "amppch.h"
 #include "SimpleDebugDevice.h"
 
+#include <windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+
 namespace AmpProcessing {
+
+    static HWAVEOUT hWaveOut = 0;
+    static WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+    static char buffer[8000 * 60] = {};
+    static WAVEHDR header;
 
     namespace Devices {
         SimpleDebugDevice::SimpleDebugDevice() : m_SampleThread(nullptr), m_Running(false), m_DebugSample(128),
             m_DeviceDetails(DeviceDetails {"Debug device", 0, 1, 128, 1024, 128, 64, 44100.f})
         {
+            waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+
+            // See http://goo.gl/hQdTi
+            for (DWORD t = 0; t < sizeof(buffer); ++t)
+                buffer[t] = static_cast<char>((((t * (t >> 8 | t >> 9) & 46 & t >> 8)) ^ (t & t >> 13 | t >> 6)) & 0xFF);
+
+            header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
         }
 
         SimpleDebugDevice::~SimpleDebugDevice()
@@ -24,13 +40,9 @@ namespace AmpProcessing {
                 auto startTime = std::chrono::high_resolution_clock::now();
                 while (m_Running) {
                     InvokeSampleReady(m_DebugSample);
-                    endTime = std::chrono::high_resolution_clock::now();
-                    deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
-                    startTime = endTime;
 
-                    auto sleep = 10 - deltaTime.count();
-                    if (sleep > 0)
-                        Sleep(sleep);
+                    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+                    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
                 };
             });
 
@@ -40,6 +52,9 @@ namespace AmpProcessing {
         bool SimpleDebugDevice::Close()
         {
             m_Running = false;
+            
+            waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+            waveOutClose(hWaveOut);
 
             return true;
         }
